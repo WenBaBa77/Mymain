5d8d2736028a8960bd3d08d8eb65a8a9ea47b2de
admin <406291764@qq.com>
2018-08-22 11:10:51 +0800

    第二次提交

---
+++ src/main/java/com/bdqn/controller/EmployeeController.java
@@ -0,0 +1,46 @@
+package com.bdqn.controller;
+
+import com.bdqn.pojo.Employee;
+import com.bdqn.service.EmployeeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.List;
+
+@Controller
+public class EmployeeController {
+    @Autowired
+    private EmployeeService userService;
+
+    @RequestMapping("/login")
+    public String login(String lastanme, String emal,Model mode){
+        System.out.println("===="+lastanme);
+        System.out.println("===="+emal);
+        if(userService.Login(lastanme,emal)){
+            mode.addAttribute("lastanme",lastanme);
+            getallemp(mode);
+            return "/WEB-INF/admin";
+        }
+        return "index";
+    }
+
+    @RequestMapping("/getall")
+    @ResponseBody
+    public String getallemp(Model model){
+        List<Employee> getallemployee = userService.getallemployee();
+        if(getallemployee!=null){
+            System.out.println("====="+getallemployee.size()+"===");
+            model.addAttribute("emplist",getallemployee);
+            for (Employee e:getallemployee) {
+                System.out.println(e.getLastName()+"=="+e.getDepartment().getDepname());
+            }
+            model.addAttribute("aa","1111111111111");
+            return "index";
+        }
+        return "index";
+    }
+}
