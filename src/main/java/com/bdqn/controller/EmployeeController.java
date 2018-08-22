901bf687d654417f698f22baaa47b4a81d4a7a87
admin <406291764@qq.com>
2018-08-22 11:17:24 +0800

    修改controller并提交

--- src/main/java/com/bdqn/controller/EmployeeController.java
+++ src/main/java/com/bdqn/controller/EmployeeController.java
@@ -16,7 +16,7 @@ public class EmployeeController {
     @Autowired
     private EmployeeService userService;
 
-    //asd
+    //asde
     @RequestMapping("/login")
     public String login(String lastanme, String emal,Model mode){
         System.out.println("===="+lastanme);
